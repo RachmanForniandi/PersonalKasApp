a9ba903f8d68b2da747bebc20b84f8f5203aeccc
RachmanForniandi <rachmanforniandi@gmail.com>
2017-06-26 13:26:09 +0700

    buat activity add

--- app/src/main/java/com/example/android/personalkasapp/AddActivity.java
+++ app/src/main/java/com/example/android/personalkasapp/AddActivity.java
@@ -1,13 +1,68 @@
 package com.example.android.personalkasapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
+
+    RadioGroup radio_status;
+    EditText et_jumlah, et_keterangan;
+    Button btn_simpan;
+    String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        status="";
+
+        radio_status  =(RadioGroup)findViewById(R.id.radio_status);
+        et_jumlah     =(EditText) findViewById(R.id.et_jumlah);
+        et_keterangan =(EditText) findViewById(R.id.et_keterangan);
+        btn_simpan    =(Button) findViewById(R.id.btn_simpan);
+
+        radio_status.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId){
+                    case R.id.radio_income:
+                        status= "MASUK";
+                        break;
+                    case R.id.radio_outcome:
+                        status= "KELUAR";
+                        break;
+                }
+                Log.d("Log Status", status);
+            }
+        });
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // CONTOH TOAST
+                Toast.makeText(AddActivity.this, "Jumlah : " + et_jumlah.getText().toString()
+                                + " Keterangan : "+ et_keterangan.getText().toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //set title
+        getSupportActionBar().setTitle("Tambahan Data");
+        //return to Home
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    //ketika icon back diclick
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
